2fbc35d68f89832df694394157c9c42319e2c724
Soren <vlad1998ss@mail.ru>
2023-07-29 22:11:55 +0300

    actions: fixing10

--- pro_platform/pro_platform/settings.py
+++ pro_platform/pro_platform/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
         'HOST': "127.0.0.1",
-        'PORT': DB_PORT_OUT,
+        'PORT': str(DB_PORT_OUT),
         "TEST": {
             "NAME": "mytestdatabase",
         },
